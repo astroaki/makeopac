8611d36d0efa99a885548815e9dc76f81fd60cb0
Akimasa <20538790+astroaki@users.noreply.github.com>
2019-09-13 12:07:44 +0900

    matdens added

--- makeopac_runfile.py
+++ makeopac_runfile.py
@@ -1,4 +1,4 @@
 import makeopac
 #dustpar={'material':'dsharp','nrang':181,'refinefact':3.0,'aminmic':0.01,'amaxmic':150.0,'na':200.}
-dustpar={'wavelengthmic':870.0,'material':'dsharp','nrang':181,'refinefact':3.0,'aminmic':0.01,'amaxmic':150.0,'na':200.}
+dustpar={'wavelengthmic':870.0,'material':'dsharp','nrang':181,'refinefact':3.0,'aminmic':0.01,'amaxmic':150.0,'na':200.,'matdens':1.675}
 makeopac.makeopac(dustpar)
